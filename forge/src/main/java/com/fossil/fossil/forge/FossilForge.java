69a480c4daac12b14d3a14aa13b03cb0badba14f
DarkPred <cookie@exedra.de>
2023-04-27 23:21:53 +0200

    Add forge config

--- forge/src/main/java/com/fossil/fossil/forge/FossilForge.java
+++ forge/src/main/java/com/fossil/fossil/forge/FossilForge.java
@@ -4,6 +4,7 @@ import com.fossil.fossil.Fossil;
 import com.fossil.fossil.client.ClientInit;
 import com.fossil.fossil.client.renderer.OverlayRenderer;
 import com.fossil.fossil.forge.capabilities.mammal.IMammalCap;
+import com.fossil.fossil.forge.config.ForgeConfig;
 import com.fossil.fossil.forge.world.biome.ForgeFossilRegion;
 import com.fossil.fossil.recipe.ModRecipes;
 import com.fossil.fossil.util.FossilFoodMappings;
@@ -16,7 +17,9 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -37,6 +40,7 @@ public class FossilForge {
         modEventBus.addListener(this::onClient);
         modEventBus.addListener(this::onCommon);
         modEventBus.addListener(this::registerCaps);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ForgeConfig.SPEC);
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> ClientInit::immediate);
     }
 
